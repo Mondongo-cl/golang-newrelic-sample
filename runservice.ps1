fc4ab68fa23f0b3cc805d94907dd1f4ba45c2f2d
Ivan Rieloff <ivan.rieloff@acemq.com>
2021-12-10 08:16:56 -0300

    fix start file

--- runservice.ps1
+++
@@ -1,8 +0,0 @@
-$env:NEWRELIC_LICENCE="7a39ccf3e6bdf7217922b5c422d18285FFFFNRAL"
-$env:MYSQL_USERNAME="blit"
-$env:MYSQL_PASSWORD="BrightlineRSWeb2020!"
-$env:MYSQL_HOST="brightline-staging-ecs-cross-account-de-dbcluster-15wnf2vaoj8rj.cluster-cv9q7b780ixy.us-east-1.rds.amazonaws.com"
-$env:MYSQL_DATABASE="customersdb"
-
-
-go run .
